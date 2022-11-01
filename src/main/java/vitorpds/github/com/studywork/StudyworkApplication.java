7e069537bf89b71855715966f64432f8404c589d
vitorpds <souza_vitor14@hotmail.com>
2022-10-31 21:34:30 -0300

    primeiro commit

---
+++ src/main/java/vitorpds/github/com/studywork/StudyworkApplication.java
@@ -0,0 +1,13 @@
+package vitorpds.github.com.studywork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vitorpds/github/com/studywork/StudyworkApplicationTests.java
@@ -0,0 +1,13 @@
+package vitorpds.github.com.studywork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
